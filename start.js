d6756aa279d1ae2cddd974c0e73d602fad920191
Artem Makhno <artem_mahno@mail.ru>
2017-08-17 17:01:56 +0300

    Readme

--- start.js
+++ start.js
@@ -4,10 +4,8 @@ function toConsole (n) {
     console.log(n);    
     tid = setTimeout(toConsole, 1000, n+1);
     if (n > 5) {
-      clearTimeout(tid);
-  }
+        clearTimeout(tid);
+    }
 }
 
 toConsole(0);
-
-246-19-31
